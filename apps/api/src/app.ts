7a5bf6f7e810f0a327284797e95a19a003dd85f5
Tanishq <88834766+Tanishq67m@users.noreply.github.com>
2026-06-22 23:48:44 +0530

    first commit

---
+++ apps/api/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (_, res) => {
+  res.json({
+    success: true,
+    message: "EventPulse API Running",
+  });
+});
+
+export default app;
